4cab1f4b14a8530a0eefd090d79890b1403f1ef9
Micah Frost <micah@mojotech.com>
2019-01-16 15:19:19 -0700

    Update schema for transportation index.

--- client/lib/schema.rb
+++ client/lib/schema.rb
@@ -1,12 +1,57 @@
 module Schema
   TRANSPORTATION = {
-    index: 'transportation',
+    index: "transportation",
     body: {
-      mappings: {
-        bus_time_record: {
-          properties: {
-            location: {
-              type: "geo_point",
+      "mappings": {
+        "bus_time_record": {
+          "properties": {
+            "actual_arrival": {
+              "type": "date",
+            },
+            "actual_departure": {
+              "type": "date",
+            },
+            "arrival_delta": {
+              "type": "long",
+            },
+            "departure_delta": {
+              "type": "long",
+            },
+            "expected_arrival": {
+              "type": "date",
+            },
+            "expected_departure": {
+              "type": "date",
+            },
+            "location": {
+              "type": "geo_point",
+            },
+            "neighborhood": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256,
+                },
+              },
+            },
+            "stop_id": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256,
+                },
+              },
+            },
+            "stop_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256,
+                },
+              },
             },
           },
         },
